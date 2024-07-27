38545c54123eb68c5788c469c137593923eb61a1
mobina-ghafari <135110634+mobina-ghafari@users.noreply.github.com>
2024-07-27 05:21:02 -0700

    App/navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './font/iranyekanwebbold.ttf';
+import './components/main.scss';
+import Navbar from "./components/navbar";
+
+
+
+function App (){
+  return <Navbar/>
+}
+
+export default App;
