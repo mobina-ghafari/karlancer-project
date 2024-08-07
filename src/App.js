6370613a5aaaf43530040f5081721c7e6a22746c
mobina-ghafari <135110634+mobina-ghafari@users.noreply.github.com>
2024-08-06 23:28:33 -0700

    App/header/responsive

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './font/iranyekanwebbold.ttf';
 import './components/main.scss';
 import Navbar from "./components/navbar";
+import Header from './components/header';
 
 
 
 
 function App (){
-  return <Navbar/>
+  return(
+    <>
+    <Navbar/>
+    <Header/>
+    </>
+  )
 }
 
 export default App;
