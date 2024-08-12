f05b4af63e33f8cc5c935a50cea2abac317f6406
mobina-ghafari <135110634+mobina-ghafari@users.noreply.github.com>
2024-08-12 09:09:40 -0700

    services

---
+++ src/components/services.jsx
@@ -0,0 +1,179 @@
+
+
+const Services =()=>{
+    return(
+        <div className="container-fluid mt-5">
+            <div className="row">
+                <div className="col-xl-9 col-lg-11 col-md-12 col-sm-12 col-xs-12 mx-auto">
+                    <h1 className="text-center">دسته بندی خدمات</h1>
+                    <div className="row">
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon1"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-vector-pen" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M10.646.646a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1 0 .708l-1.902 1.902-.829 3.313a1.5 1.5 0 0 1-1.024 1.073L1.254 14.746 4.358 4.4A1.5 1.5 0 0 1 5.43 3.377l3.313-.828zm-1.8 2.908-3.173.793a.5.5 0 0 0-.358.342l-2.57 8.565 8.567-2.57a.5.5 0 0 0 .34-.357l.794-3.174-3.6-3.6z"/>
+            <path fillRule="evenodd" d="M2.832 13.228 8 9a1 1 0 1 0-1-1l-4.228 5.168-.026.086z"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">طراحی و گرافیک</div>
+                          <div className="my-card-text">طرح های خلاقانه برای کار خود سفارش دهید و در میان همه بدرخشید </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon2"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-file-earmark-medical" viewBox="0 0 16 16">
+            <path d="M7.5 5.5a.5.5 0 0 0-1 0v.634l-.549-.317a.5.5 0 1 0-.5.866L6 7l-.549.317a.5.5 0 1 0 .5.866l.549-.317V8.5a.5.5 0 1 0 1 0v-.634l.549.317a.5.5 0 1 0 .5-.866L8 7l.549-.317a.5.5 0 1 0-.5-.866l-.549.317zm-2 4.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
+            <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+          </svg></div>
+                        </div>
+                          <div className="h5"> تولید محتوا</div>
+                          <div className="my-card-text"> کار های نوشتاری و تایپ خود را به تولید کننده های محتوای کارلنسر بدخید تا نتیجه باور نکردنی بگیرید </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon3"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-translate" viewBox="0 0 16 16">
+            <path d="M4.545 6.714 4.11 8H3l1.862-5h1.284L8 8H6.833l-.435-1.286zm1.634-.736L5.5 3.956h-.049l-.679 2.022z"/>
+            <path d="M0 2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zm7.138 9.995q.289.451.63.846c-.748.575-1.673 1.001-2.768 1.292.178.217.451.635.555.867 1.125-.359 2.08-.844 2.886-1.494.777.665 1.739 1.165 2.93 1.472.133-.254.414-.673.629-.89-1.125-.253-2.057-.694-2.82-1.284.681-.747 1.222-1.651 1.621-2.757H14V8h-3v1.047h.765c-.318.844-.74 1.546-1.272 2.13a6 6 0 0 1-.415-.492 2 2 0 0 1-.94.31"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">ترجمه</div>
+                          <div className="my-card-text">ترجمه های خود را به بهترین مترم های کارلنسر بسپارید </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon4"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-film" viewBox="0 0 16 16">
+            <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">عکس،انیمیشن و ویدئو </div>
+                          <div className="my-card-text">پروژه های عکاسی، فیلم برداری یا ادیت عکس و فیلم خود را به بهترین فریلنسرهای این حوزه بسپارید </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon5"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-card-heading" viewBox="0 0 16 16">
+            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+            <path d="M3 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0-5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5z"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">طراحی سایت </div>
+                          <div className="my-card-text">برای کسب و کار خود،یک وبسایت بسازید تا در اینترنت معروف شوید ومشتریان تان شما را پیدا کنند </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon6"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-hash" viewBox="0 0 16 16">
+            <path d="M8.39 12.648a1 1 0 0 0-.015.18c0 .305.21.508.5.508.266 0 .492-.172.555-.477l.554-2.703h1.204c.421 0 .617-.234.617-.547 0-.312-.188-.53-.617-.53h-.985l.516-2.524h1.265c.43 0 .618-.227.618-.547 0-.313-.188-.524-.618-.524h-1.046l.476-2.304a1 1 0 0 0 .016-.164.51.51 0 0 0-.516-.516.54.54 0 0 0-.539.43l-.523 2.554H7.617l.477-2.304c.008-.04.015-.118.015-.164a.51.51 0 0 0-.523-.516.54.54 0 0 0-.531.43L6.53 5.484H5.414c-.43 0-.617.22-.617.532s.187.539.617.539h.906l-.515 2.523H4.609c-.421 0-.609.219-.609.531s.188.547.61.547h.976l-.516 2.492c-.008.04-.015.125-.015.18 0 .305.21.508.5.508.265 0 .492-.172.554-.477l.555-2.703h2.242zm-1-6.109h2.266l-.515 2.563H6.859l.532-2.563z"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">دیجیتال مارکتینگ </div>
+                          <div className="my-card-text">بازده شرکت خود را با استخدام یک بازاریاب چندین برابر زیاد کنید </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon7"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">سئو SEO</div>
+                          <div className="my-card-text"> به جای صرف هزینه های گزاف روی تبلیغات، با سئو و بهینه سازی سایت تان، همیشه در رتبه های بالا باشید </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon8"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-code-slash" viewBox="0 0 16 16">
+            <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0m6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">برنامه نویسی،نرم افزار و...</div>
+                          <div className="my-card-text">برای کسب و کار خود، نرم افزار یا اپلیکیشن سفارش دهید تا مشتری های خود را چندین برابر کنید </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon9"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-headset" viewBox="0 0 16 16">
+            <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">منشی و دستیار مجازی </div>
+                          <div className="my-card-text">کارهای مهم خود را با آسایش به منشی و دستیار مجازی خود بسپارید </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon10"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-music-note-beamed" viewBox="0 0 16 16">
+            <path d="M6 13c0 1.105-1.12 2-2.5 2S1 14.105 1 13s1.12-2 2.5-2 2.5.896 2.5 2m9-2c0 1.105-1.12 2-2.5 2s-2.5-.895-2.5-2 1.12-2 2.5-2 2.5.895 2.5 2"/>
+            <path fillRule="evenodd" d="M14 11V2h1v9zM6 3v10H5V3z"/>
+            <path d="M5 2.905a1 1 0 0 1 .9-.995l8-.8a1 1 0 0 1 1.1.995V3L5 4z"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">گویندگی،صوت و موسیقی </div>
+                          <div className="my-card-text">با استخدام یک گوینده به تبلیغات و ویدیوهای خود صدایی به یادماندنی اضافه کنید </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon11"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-cloud" viewBox="0 0 16 16">
+            <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383m.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">مهندسی و علوم تجربی </div>
+                          <div className="my-card-text">پروژه های در زمینه مهندسی، علوم تجربی و... را به بهترین فریلنسرها بسپارید </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                      <div className="col-xl-3 col-lg-4 col-md-6 ">
+                       <a href="./" className="my-card">
+                        <div>
+                        <div className="my-card-icon12"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-briefcase" viewBox="0 0 16 16">
+            <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5m1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0M1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5"/>
+          </svg></div>
+                        </div>
+                          <div className="h5">سایر کسب و کارها </div>
+                          <div className="my-card-text">رفع سایر نیازهای شما به سرعت و کمترین هزینه </div>
+                        <button className="my-card-btn">مشاهده جزئیات</button>
+                       </a>
+                      </div>
+                    </div>
+                    <div className=""><button className="my-end-btn">مشاهده همه مهارت های دورکاری</button></div>
+                    <div className="my-end-banner">
+                      <h2 className="text-white">کسب درآمد با فریلنسینگ</h2>
+                      <button className="my-end-banner-btn">
+                        <span className="my-end-banner-btn-span ">درآمد خود را آغاز کنید</span>
+                        <div className="my-end-banner-btn-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
+                         <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+                        </svg>
+                        </div>
+                      </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Services;
