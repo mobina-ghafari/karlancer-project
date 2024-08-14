735930e03bea72cd74b6b2a3b3ac35b0a068238f
mobina-ghafari <135110634+mobina-ghafari@users.noreply.github.com>
2024-08-14 03:12:42 -0700

    freelancer

---
+++ src/components/freelancer.jsx
@@ -0,0 +1,39 @@
+
+
+const Freelancer =()=>{
+    return(
+        <>
+        <div className="container-fluid my-section">
+            <h2 className="text-center text-white">فریلنسر کیست؟</h2>
+            <div className="row">
+                <div className="col-xl-9 col-lg-11 col-md-12 col-sm-12 col-xs-12 mx-auto">
+                <div className="my-freelancer-text">
+                    <p className="my-freelancer-p">
+                    فریلنسر یا آزادکار، شخصیست که با توجه به مهارت‌ها و تخصصی که دارد، به صورت اینترنتی و دورکاری، پروژه میگیرد و انجام میدهد و برای کارفرما می‌فرستد. فریلنسرها از طریق پروژه های دورکاری کسب درآمد می‌کنند. به این شیوه کاری فریلنسینگ گفته می شود. فریلنسرها، میتوانند از هر جایی که مایلند، حتی در خانه و هر ساعتی که دوست دارند، کار کنند و پروژه انجام دهند. فریلنسر ها ساعت کاری و محل کار ثابتی ندارند و خودشان محل و ساعات کاریشون رو انتخاب میکنند. فریلنسینگ نوعی سبک زندگی و کار است که امکان دورکاری و کسب درآمد از منزل رو برای فریلنسرها فراهم میکنه . بنابر بررسی‌ها تا سال 2025، نصف جمعیت شاغلان دنیا رو فریلنسرها تشکیل میدن. اگر شما هم تخصص یا مهارتی دارید، به فریلنسرهای کارلنسر بپیوندید و کسب درآمد اینترنتی داشته باشید. کارلنسر به عنوان یک سایت فریلنسینگ امکان کسب درآمد از طریق دورکاری رو برای فریلنسرها فراهم کرده. همچنین برونسپاری پروژه های طراحی وب و برنامه نویسی، تولید محتوا، طراحی گرافیک، تایپ، ترجمه و بسیاری مهارت های دیگه هم از طریق کارلنسر امکان پذیره. اگر مایلید فریلنسر بشید و از طریق دورکاری کسب درآمد کنید یا با برونسپاری پروژه هاتون رو انجام بدید همین امروز به کارلنسر بپیوندید.
+                    </p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div className="container-fluid mt-4">
+            <div className="row">
+                <div className="col-xl-9 col-lg-11 col-md-12 col-sm-12 col-xs-12 mx-auto">
+                <div className="my-end-banner3">
+                      <h2 className="text-white">کارتون رو به حرفه ای ها بسپارید</h2>
+                      <button className="my-end-banner-btn3">
+                        <span className="my-end-banner-btn-span3 ">پروژه خود را ثبت کنید</span>
+                        <div className="my-end-banner-btn-icon3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
+                         <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+                        </svg>
+                        </div>
+                      </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Freelancer;
